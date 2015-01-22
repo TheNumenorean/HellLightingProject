b17190772e0b73db086ab20a6eed5be1c55b4eeb
Francesco Macagno <fjmacagno1@yahoo.com>
2015-01-22 15:05:35 -0800

    Fixed file structure

--- Arduino/LEDEthernetServer/LEDEthernetServer.ino
+++ Arduino/LEDEthernetServer/LEDEthernetServer.ino
@@ -8,14 +8,10 @@
 // How many leds in your strip?
 #define NUM_LEDS 1200
 
-// For led chips like Neopixels, which have a data line, ground, and power, you just
-// need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
-// ground, and power), like the LPD8806, define both DATA_PIN and CLOCK_PIN
 #define DATA_PIN_1 7
 #define DATA_PIN_2 6
 #define POT_PIN A10
 
-// Define the array of leds
 CRGB leds[NUM_LEDS];
 
 byte mac[] = {  
