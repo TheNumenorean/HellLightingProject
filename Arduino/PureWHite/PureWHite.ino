0130d155db0b0767debf45997f5bde5e9d9d208d
Francesco Macagno <fjmacagno1@yahoo.com>
2015-04-25 15:20:10 -0700

    Adjusting for larger led numbers

--- Arduino/PureWHite/PureWHite.ino
+++ Arduino/PureWHite/PureWHite.ino
@@ -1,26 +1,28 @@
 #include "FastLED.h"
 
 // How many leds in your strip?
-#define NUM_LEDS 600
+#define NUM_LEDS 1200
 
 // For led chips like Neopixels, which have a data line, ground, and power, you just
 // need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
 // ground, and power), like the LPD8806, define both DATA_PIN and CLOCK_PIN
-#define DATA_PIN 7
+#define DATA_PIN_1 2
+#define DATA_PIN_2 3
 
 // Define the array of leds
 CRGB leds[NUM_LEDS];
 
 void setup() { 
   
-  FastLED.addLeds<APA104, DATA_PIN, GRB>(leds, NUM_LEDS);
-  FastLED.addLeds<APA104, 6, GRB>(leds, NUM_LEDS);
+  FastLED.addLeds<APA104, DATA_PIN_1, GRB>(leds, 0, NUM_LEDS / 2);
+  FastLED.addLeds<APA104, DATA_PIN_2, GRB>(leds, NUM_LEDS / 2, NUM_LEDS / 2);
+
   
   
   for(int i = 0; i < NUM_LEDS; i++){
-      leds[i].r = 0;
-      leds[i].g = 0;
-      leds[i].b = 0;
+      leds[i].r = 255;
+      leds[i].g = 255;
+      leds[i].b = 255;
   }
   
   
