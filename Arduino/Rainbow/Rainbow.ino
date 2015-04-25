3bc24e4faff075f19ba75f2ae1b5a0e6e1959aff
Francesco Macagno <fjmacagno1@yahoo.com>
2015-04-25 15:20:22 -0700

    New wiring

--- Arduino/Rainbow/Rainbow.ino
+++ Arduino/Rainbow/Rainbow.ino
@@ -6,9 +6,9 @@
 // For led chips like Neopixels, which have a data line, ground, and power, you just
 // need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
 // ground, and power), like the LPD8806, define both DATA_PIN and CLOCK_PIN
-#define DATA_PIN_1 7
-#define DATA_PIN_2 6
-#define POT_PIN A10
+#define DATA_PIN_1 2
+#define DATA_PIN_2 3
+#define POT_PIN A0
 
 // Define the array of leds
 CRGB leds[NUM_LEDS];
@@ -16,6 +16,8 @@ CRGB leds[NUM_LEDS];
 void setup() {
 
   Serial.begin(9600);
+  
+  Serial.println("Loading");
 
   FastLED.addLeds<APA104, DATA_PIN_1, GRB>(leds, 0, NUM_LEDS / 2);
   FastLED.addLeds<APA104, DATA_PIN_2, GRB>(leds, NUM_LEDS / 2, NUM_LEDS / 2);
